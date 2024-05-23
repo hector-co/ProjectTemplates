3379301edaec636302f559bf2da4f96bd140aa5f
Hector Carballo <hectorcarballo.hco@gmail.com>
2024-05-23 18:28:22 -0400

    updating references

--- CqrsModuleTemplate_Result/TplModule/TplModule.Application/GlobalUsings.cs
+++ CqrsModuleTemplate_Result/TplModule/TplModule.Application/GlobalUsings.cs
@@ -1,3 +1,4 @@
 global using Shared.Domain;
 global using Shared.Application.Commands;
 global using Shared.Application.Queries;
+global using LanguageExt.Common;
--- CqrsModuleTemplate_Result/TplModule/TplModule.Infrastructure/GlobalUsings.cs
+++ CqrsModuleTemplate_Result/TplModule/TplModule.Infrastructure/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Shared.Application.Behaviors;
 global using Shared.Application.Commands;
 global using Shared.Application.Queries;
 global using Shared.Infrastructure.DataAccess.EF;
+global using LanguageExt.Common;
