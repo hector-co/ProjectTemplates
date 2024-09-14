2b591002deb49a32b2edb1d2c1b8ce3e269e22e1
Hector Carballo <hectorcarballo.hco@gmail.com>
2024-09-14 01:26:27 -0400

    mapster to global using

--- CqrsModuleTemplate_Result/TplModule/TplModule.Infrastructure/GlobalUsings.cs
+++ CqrsModuleTemplate_Result/TplModule/TplModule.Infrastructure/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using Shared.Application.Commands;
 global using Shared.Application.Queries;
 global using Shared.Infrastructure.DataAccess.EF;
 global using LanguageExt.Common;
+global using Mapster;
